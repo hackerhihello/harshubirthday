d4326b581ce9cfd16af24b3bfc31be659db4ca8e
hackerhihello <hackerhihello123@gmail.com>
2023-11-26 11:08:52 +0530

    birthday page change

--- src/Pages/Birthdaywish.js
+++ src/Pages/Birthdaywish.js
@@ -1,23 +1,72 @@
 import React, { useState } from 'react';
 import birthday from '../Assets/birthday.jpg';
-import harshu from '../Assets/harshu.jpg'
+import harshu from '../Assets/harshu.jpg';
+import harshi from '../Assets/harshi.jpg';
+import harsh from '../Assets/harsh.jpg';
+import harshiy from '../Assets/harshiy.jpg';
+import harshiym from '../Assets/harshiym.jpg';
+import HY from '../Assets/HY.mp4';
 
 export const Birthdaywish = () => {
   const [imageSrc, setImageSrc] = useState(birthday);
-  const birthdayWish = "Happy Birthday to the most amazing sister in the world! 🎂🎉";
-   
-  const handleClick = () => {
-    // Change the image source when the button is clicked
+  const [imageContent, setImageContent] = useState("Happy");
+
+  const [image, setImage] = useState(harsh);
+  const [imageContent2, setImageContent2] = useState("Birthday ");
+
+  const [imageh, setImageh] = useState(harshiy);
+  const [imageContent3, setImageContent3] = useState("Hrshu");
+
+  const handleImageChange = () => {
     setImageSrc(imageSrc === birthday ? harshu : birthday);
+    setImageContent("Happy Birthday to the most amazing sister in the world! 🎂🎉");
   };
+
+  const handleImage = () => {
+    setImage(image === harsh ? harshi : harsh);
+    setImageContent2("Wishing you a day filled with joy and laughter! 🎈🎁");
+  };
+
+  const handle = () => {
+    setImageh(imageh === harshiy ? harshiym : harshiy);
+    setImageContent3("May your day be as special as you are! 🌟🎂");
+  };
+
   return (
     <>
-      <div className="card" >
-        <img src={imageSrc} className="card-img-top"  />
+      <div className="card">
+        <img
+          src={imageSrc}
+          className="card-img-top"
+          onClick={handleImageChange}
+          onTouchStart={handleImageChange}
+        />
+        {/* <h5 className="card-title">Happy Birthday</h5> */}
+        <p className="card-text">{imageContent}</p>
+
+        <img
+          src={image}
+          className="card-img-top"
+          onClick={handleImage}
+          onTouchStart={handleImage}
+        />
+        {/* <h5 className="card-title">Happy Birthday</h5> */}
+        <p className="card-text">{imageContent2}</p>
+
+        <img
+          src={imageh}
+          className="card-img-top"
+          onClick={handle}
+          onTouchStart={handle}
+        />
+        {/* <h5 className="card-title">Happy Birthday</h5> */}
+        <p className="card-text">{imageContent3}</p>
+
+        <video controls className="card-img-top">
+          <source src={HY} type="video/mp4" />
+        </video>
         <div className="card-body">
-          <h5 className="card-title">Happy Birthday</h5>
-          <p className="card-text">{birthdayWish}</p>
-          <button onClick={handleClick} className="btn btn-primary">Click Here</button>
+          {/* Additional content if needed */}
         </div>
       </div>
     </>
